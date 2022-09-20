9353d3c4c1d9d22e0edce6dce77c39d06a4ac98d
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2022-09-20 09:36:43 +0530

    Add files via upload

---
+++ copyonestringtoanotherstrng.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    char text1[MAX_SIZE];
+    char text2[MAX_SIZE];
+    int i;
+
+
+    printf("Enter any string: ");
+    gets(text1);
+
+
+    i=0;
+    while(text1[i] != '\0')
+    {
+        text2[i] = text1[i];
+        i++;
+    }
+
+
+    text2[i] = '\0';
+
+    printf("First string = %s\n", text1);
+    printf("Second string = %s\n", text2);
+    printf("Total characters copied = %d\n", i);
+
+    return 0;
+}
